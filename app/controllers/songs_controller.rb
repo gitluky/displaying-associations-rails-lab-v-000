8a24f71d33bcf4c54992feecd858bf599768a155
Jonathan Luk <jluk87@gmail.com>
2019-04-06 22:45:57 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,11 +22,10 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+
   end
 
   def update
-    @song = Song.find(params[:id])
 
     @song.update(song_params)
 
