eb6af4aa38097d9f44ee534279cc2f2221bad046
Jonathan Luk <jluk87@gmail.com>
2019-04-06 23:06:33 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,7 +3,7 @@ class ArtistsController < ApplicationController
   before_action :set_artist, only: [:show, :edit, :update, :destry]
 
   def index
-    @artist = Artist.all
+    @artists = Artist.all
   end
 
   def show
